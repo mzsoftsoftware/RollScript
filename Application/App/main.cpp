d1e5b23c270f2e239be5ec38553abc0f819ae95f
Michael Zeller <michael@Dekstop-EndeavourOS>
2026-09-07 14:12:59 +0200

    Main Icon, Structured CMakeLists.txt

--- Application/App/main.cpp
+++ Application/App/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char* argv[])
     qInstallMessageHandler(messageHandler);
 
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral(":/Application/RollScript.svg")));
 
     //qDebug() << "qDebug funktioniert";
     //qInfo() << "qInfo funktioniert";
