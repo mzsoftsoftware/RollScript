52104d22f2a8ecbf291c092b678a18c3288f98fa
Michael Zeller <michael@Dekstop-EndeavourOS>
2026-09-17 16:05:45 +0200

    Initial structure

---
+++ Plugins/Features/RollScriptBlockImage/RollScriptBlockImageFeaturePlugin.cpp
@@ -0,0 +1,37 @@
+#include "RollScriptBlockTextFeaturePlugin.h"
+
+#include "RollScriptBlockTextWidget.h"
+#include "RollScriptBlockTextDocument.h"
+#include "RollScriptBlockTextRenderer.h"
+
+#include <QFile>
+#include <QDir>
+
+RollScriptBlockTextFeaturePlugin::RollScriptBlockTextFeaturePlugin(QObject* parent)
+    : IFeaturePlugin(parent)
+{
+    const QString qstrPath =
+        QStringLiteral(
+            ":/RollScriptBlockTextFeaturePlugin/icons/FeatureBlockText.icon");
+
+    qDebug() << "Constructor Plugin icon:"
+             << QFile::exists(qstrPath)
+             << QIcon(qstrPath).isNull();
+
+    qDebug() << "Constructor resources:" << QDir(QStringLiteral(":/")).entryList();
+}
+
+RollScriptBlockWidgetBase* RollScriptBlockTextFeaturePlugin::createFeatureBlockWidget(QWidget* parent)
+{
+    return new RollScriptBlockTextWidget(m_pluginInfo.pluginId(), m_pluginInfo.featureBlockName(), parent);
+}
+
+RollScriptBlockDocumentBase* RollScriptBlockTextFeaturePlugin::createFeatureBlockDocument(QObject* parent)
+{
+    return new RollScriptBlockTextDocument(m_pluginInfo.pluginId(), parent);
+}
+
+RollScriptBlockRendererBase* RollScriptBlockTextFeaturePlugin::createFeatureBlockRenderer(QObject* parent)
+{
+    return new RollScriptBlockTextRenderer(m_pluginInfo.pluginId(), parent);
+}
